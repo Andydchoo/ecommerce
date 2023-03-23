508a2b012071ffdb6a09e336d15c411aba38063a
Andy Choo <andydchoo@gmail.com>
2023-03-22 22:57:22 -0400

    added carousel

--- client/src/scenes/home/Home.jsx
+++ client/src/scenes/home/Home.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from 'react';
+import MainCarousel from "./MainCarousel";
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="home">
+      <MainCarousel />
+    </div>
   )
 }
